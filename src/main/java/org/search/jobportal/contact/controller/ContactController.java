f42d149771931d25c83823379d6d043edb63f882
Ajas Bakran <ajas.bakran@pubmatic.com>
2026-05-10 12:23:01 +0530

    Added Contact Us controller

---
+++ src/main/java/org/search/jobportal/contact/controller/ContactController.java
@@ -0,0 +1,28 @@
+package org.search.jobportal.contact.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.search.jobportal.contact.dto.ContactRequestDto;
+import org.search.jobportal.contact.service.IContactService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contacts")
+@RequiredArgsConstructor
+public class ContactController {
+    private final IContactService contactService;
+
+    @PostMapping(version = "1.0")
+    public ResponseEntity<String> saveContactMsg(@RequestBody ContactRequestDto contactRequestDto) {
+        boolean isSaved = contactService.saveContact(contactRequestDto);
+
+        if (isSaved) {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body("Contact Created");
+        } else  {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Contact Not Saved");
+        }
+    }
+}
